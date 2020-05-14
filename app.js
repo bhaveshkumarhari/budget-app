104cb7ae3c1ecc0e8483ed02d7a22304f204a82c
bhaveshkumarhari <bhaveshkumar.hari@gmail.com>
2020-05-14 22:04:50 +0100

    Created initialization function

--- app.js
+++ app.js
@@ -36,30 +36,39 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgerCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {     
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem()
+            }
+        });
+    };
+
+    
 
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
         var input = UICtrl.getinput();
-        console.log(input);
 
         // 2. Add the item to the budget controller
         // 3. Display the item on the UI
         // 4. Calculate the budget
         // 5. Display the budget on the UI
 
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem()
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListners();
         }
-        
-
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
