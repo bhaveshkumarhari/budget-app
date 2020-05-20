c396372c33068375cf01530be5e465a47eae20a6
bhaveshkumarhari <bhaveshkumar.hari@gmail.com>
2020-05-20 13:07:23 +0100

    Logic to convert string to number and cannot enter empty field values

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -143,7 +143,15 @@ var controller = (function(budgerCtrl, UICtrl) {
         });
     };
 
-    
+    var updateBudget = function() {
+
+        // 1. Calculate the budget
+
+        // 2. return the budget
+
+        // 3. Display the budget on the UI
+
+    };
 
     var ctrlAddItem = function() {
 
@@ -152,17 +160,21 @@ var controller = (function(budgerCtrl, UICtrl) {
         // 1. Get the field input data
         input = UICtrl.getinput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgerCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
 
-        // 3. Display the item on the UI
-        UICtrl.addListItem(newItem, input.type);
+            // 2. Add the item to the budget controller
+            newItem = budgerCtrl.addItem(input.type, input.description, input.value);
 
-        // 4. Clear the fields
-        UICtrl.clearFields();
+            // 3. Display the item on the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        // 5. Calculate the budget
-        // 6. Display the budget on the UI
+            // 4. Clear the fields
+            UICtrl.clearFields();
+
+            // 5. Calculate and update budget
+            updateBudget();
+
+        }
 
     };
 
